4499587dd21917bd1da83b5d15498332f72b5fef
Carlos Ramos <carlosefrfloresta@gmail.com>
2020-06-13 15:05:24 -0300

    Added screens

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -4,7 +4,7 @@ import { AppComponent } from './app.component';
 describe('AppComponent', () => {
   it('should render the component', async () => {
     const { getByText } = await render(AppComponent);
-    const text = getByText('Welcome')
+    const text = getByText('taskbox')
     expect(text);
   });
 });
